1227a634a0712352d86e45d28e690c34d8cb08a1
ivandr <ivandr@bk.ru>
2023-02-08 14:01:51 +0300

    refactor: убрал неиспользуемые методы

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -6,8 +6,6 @@ import ru.yandex.practicum.filmorate.error.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
 import javax.validation.Valid;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,7 +13,7 @@ import java.util.Set;
 @RequestMapping("/users")
 @Slf4j
 public class UserController {
-    private Set<User> users = new HashSet<>();
+    private final Set<User> users = new HashSet<>();
 
     private int genID() {
         int maxid = 1;
@@ -32,10 +30,9 @@ public class UserController {
     }
 
     @PostMapping
-    public User createUser(@Valid @RequestBody User user) throws ValidationException {
+    public User createUser(@Valid @RequestBody User user) {
         user.setId(genID());
-        convert(user);
-        //validate(user);
+        compute(user);
 
         users.add(user);
         log.info("Пользователь добавлен " + user);
@@ -44,8 +41,7 @@ public class UserController {
 
     @PutMapping
     public User updateUser(@Valid @RequestBody User user) throws ValidationException {
-        convert(user);
-        //validate(user);
+        compute(user);
         if (!users.contains(user)) {
             String error = "Пользователь " + user.getId() + " не найден";
             log.error(error);
@@ -58,38 +54,9 @@ public class UserController {
         return user;
     }
 
-    private void convert(User user) {
+    private void compute(User user) {
         if (user.getName() == null || user.getName().isBlank()) {
             user.setName(user.getLogin());
-        }
-    }
-
-    private void validate(User user) throws ValidationException {
-        if (user.getEmail() == null || user.getEmail().isBlank()) {
-            String error = "Email пользователя должен быть задан";
-            log.error(error);
-            throw new ValidationException(error);
-        }
-
-        final String emailSymbol = "@";
-        if (!user.getEmail().contains(emailSymbol)) {
-            String error = "Email пользователя должен сожержать символ " + emailSymbol;
-            log.error(error);
-            throw new ValidationException(error);
-        }
-
-        if (user.getLogin() == null || user.getLogin().isBlank()) {
-            String error = "Логин пользователя должен быть задан";
-            log.error(error);
-            throw new ValidationException(error);
-        }
-
-        final LocalDate toDay = LocalDate.now();
-        final DateTimeFormatter format = DateTimeFormatter.ofPattern("dd:MM:yyyy");
-        if (user.getBirthday() != null && user.getBirthday().isAfter(toDay)) {
-            String error = "День рожденье пользователя не может быть больше текущей даты " + toDay.format(format);
-            log.error(error);
-            throw new ValidationException(error);
         }
     }
 }
