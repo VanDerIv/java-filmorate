6f7121d3c798be2ebe77e65893775d3eb070149a
Ekaterina <katya-3105@yandex.ru>
2023-04-11 23:10:26 +0300

    feat: тз12 Удаление фильмов и пользователей

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -5,11 +5,18 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.List;
 
 public interface UserStorage {
+
     List<User> getUsers();
+
     User getUser(Long id);
+
     User createUser(User user);
+
     User updateUser(User user);
+
     void deleteUser(Long id);
+
     void addUserToFriend(User user, User friend);
+
     void removeUserFromFriend(User user, User friend);
 }
