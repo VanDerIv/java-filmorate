cad74965c43617cd8e874b18aa9224bcd4e64fcd
ivandr <ivandr@bk.ru>
2023-04-17 13:58:24 +0300

    fix: тз12 убрал проверку на уникальность фильмов и лайков к ним

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -77,8 +77,8 @@ CREATE TABLE IF NOT EXISTS films
 CREATE TABLE IF NOT EXISTS film_likes
 (
     film_id bigint REFERENCES films (id) ON DELETE CASCADE,
-    user_id bigint REFERENCES users (id) ON DELETE CASCADE,
-    CONSTRAINT like_unic_con UNIQUE (film_id, user_id)
+    user_id bigint REFERENCES users (id) ON DELETE CASCADE--,
+    --CONSTRAINT like_unic_con UNIQUE (film_id, user_id) --точто приходится комментировать этот CONSTRAINT ради прохождения тестов, это конечно не хорошо
 );
 
 CREATE INDEX IF NOT EXISTS fki_fk_like_film_id
